7824b1c142c314844934cbc5a68f36acee20855f
yangpengp <yangpengproject@163.com>
2020-07-07 11:42:42 +0800

    服务器代码修改

--- store/Server.js
+++ store/Server.js
@@ -2,32 +2,32 @@ const http = require('http');
 const fs = require('fs');
 const url = require('url');
 // 创建服务器
-http.createServer( function (request, response) {  
+http.createServer( function (req, res) {  
    // 解析请求，包括文件名
    let pathname = '';
-   if(url.parse(request.url).pathname == '/'){
+   if(url.parse(req.url).pathname == '/'){
        pathname = 'public/index.html'
    }else{
-    pathname = 'public' + url.parse(request.url).pathname;
+    pathname = 'public' + url.parse(req.url).pathname;
    }
    // 输出请求的文件名
-   console.log("Request for " + pathname + " received.");
+   console.log("req for " + pathname + " received.");
    // 从文件系统中读取请求的文件内容
    fs.readFile(pathname.substr(0), function (err, data) {
       if (err) {
          console.log(err);
          // HTTP 状态码: 404 : NOT FOUND
          // Content Type: text/html
-         response.writeHead(404, {'Content-Type': 'text/html'});
+         res.writeHead(404, {'Content-Type': 'text/html'});
       }else{             
          // HTTP 状态码: 200 : OK
          // Content Type: text/html
-         response.writeHead(200, {'Content-Type': 'text/html'});       
+         res.writeHead(200, {'Content-Type': 'text/html'});       
          // 响应文件内容
-         response.write(data.toString());        
+         res.write(data.toString());        
       }
       //  发送响应数据
-      response.end();
+      res.end();
    });   
 }).listen(3000);
 // 控制台会输出以下信息
